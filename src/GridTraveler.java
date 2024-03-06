f91f3e2d09790ab894f00f7d6786ad602db408c2
Andrew Lester <andrewjlester@hotmail.com>
2024-03-05 21:51:14 -0500

    Adding grid traveler

---
+++ src/GridTraveler.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class GridTraveler {
+    HashMap<String, Long> memo = new HashMap<>();
+
+    public long calculate(int rows, int columns) {
+        String memoKey = rows + "," + columns;
+        if(this.memo.containsKey(memoKey)) return this.memo.get(memoKey);
+
+        if(rows == 0 || columns == 0) return 0;
+        if(rows == 1 && columns == 1) return 1;
+
+        this.memo.put(memoKey, calculate(rows - 1, columns) + calculate(rows, columns - 1));
+        return this.memo.get(memoKey);
+    }
+
+}
---
+++ test/GridTravelerTest.java
@@ -0,0 +1,58 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class GridTravelerTest {
+
+    @org.junit.jupiter.api.Test
+    void calculate0or0returns0() {
+        GridTraveler gridTraveler = new GridTraveler();
+
+        long results = gridTraveler.calculate(0,0);
+        assertEquals(0, results);
+
+        results = gridTraveler.calculate(1,0);
+        assertEquals(0, results);
+
+        results = gridTraveler.calculate(0,1);
+        assertEquals(0, results);
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculate1and1returns1() {
+        GridTraveler gridTraveler = new GridTraveler();
+
+        long results = gridTraveler.calculate(1,1);
+        assertEquals(1, results);
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculate2and3returns3() {
+        GridTraveler gridTraveler = new GridTraveler();
+
+        long results = gridTraveler.calculate(2,3);
+        assertEquals(3, results);
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculate3and2returns3() {
+        GridTraveler gridTraveler = new GridTraveler();
+
+        long results = gridTraveler.calculate(3,2);
+        assertEquals(3, results);
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculate3and3returns3() {
+        GridTraveler gridTraveler = new GridTraveler();
+
+        long results = gridTraveler.calculate(3,3);
+        assertEquals(6, results);
+    }
+
+    @org.junit.jupiter.api.Test // 16 seconds
+    void calculate18and18returns2333606220() {
+        GridTraveler gridTraveler = new GridTraveler();
+
+        long results = gridTraveler.calculate(18,18);
+        assertEquals(2333606220l, results);
+    }
+}
